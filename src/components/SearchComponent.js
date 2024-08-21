b3e50e31e81aa353d5ffa1e5c34922f2555f1a57
Aybars TÜREL <aybarsturel@192.168.1.100>
2024-08-22 00:14:51 +0300

    Search can suggest according to input

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,7 +1,5 @@
-// components/SearchComponent.js
-
 import React from 'react';
-import { Input } from 'antd';
+import { Autocomplete, TextField } from '@mui/material';
 import { useMap } from 'react-leaflet';
 
 const DEFAULT_ZOOM = 14;
@@ -9,25 +7,51 @@ const DEFAULT_ZOOM = 14;
 function SearchComponent({ markers, setSelectedMarker, setClosedByMapClick }) {
   const map = useMap();
 
-  const onSearch = (value) => {
-    const marker = markers.find(marker => marker.title.toLowerCase().includes(value.toLowerCase()));
-    if (marker) {
-      setSelectedMarker(marker);
+  // Map markers to the format expected by Autocomplete
+  const items = markers.map((marker, index) => ({
+    id: index,
+    title: marker.title,
+    marker: marker,
+  }));
+
+  const handleOnSelect = (event, value) => {
+    if (value) {
+      const selectedMarker = markers[value.id];
+      setSelectedMarker(selectedMarker);
       setClosedByMapClick(false);
-      // Center the map on the found marker
-      map.setView(marker.geocode, DEFAULT_ZOOM);
+      // Center the map on the selected marker
+      map.setView(selectedMarker.geocode, DEFAULT_ZOOM);
     } else {
       console.log("No matching marker found");
     }
   };
 
   return (
-    <Input.Search
-      className="search-container"
-      placeholder="Please enter the research facility or lab name."
-      onSearch={onSearch}
-      enterButton
-    />
+    <div className="search-container">
+      <Autocomplete size="small"
+        options={items}
+        getOptionLabel={(option) => option.title}
+        onChange={handleOnSelect}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Please enter the research facility or lab name."
+            variant="outlined"
+            autoFocus
+            sx={{
+              
+              backgroundColor: 'white', // Ensure background is white
+              '& .MuiInputBase-input': {
+                padding: '10px',    // Adjust the padding inside the input
+              },
+            }}
+          />
+        )}
+        sx={{
+          height: '1px'
+        }}
+      />
+    </div>
   );
 }
 

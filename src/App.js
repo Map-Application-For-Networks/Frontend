fa18147a84c4c059996a7ffca67307cfd310afc3
Meryem Erdoğan <114259664+meryemerdogan@users.noreply.github.com>
2024-09-11 20:48:45 +0300

    Routers added for Signup and Login Pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter , Routes, Route} from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import AddMarkerPage from './pages/AddMarkerPage';
 import ConfimationPage from './pages/ConfirmationPage';
-
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
 
@@ -13,6 +14,8 @@ function App() {
         <Route path="/" element={<MainPage />}> </Route>
         <Route path="/addmarker" element={<AddMarkerPage />}> </Route>
         <Route path="/confirmation" element ={<ConfimationPage />}> </Route>
+        <Route path="/login" element={<LoginPage />}> </Route>
+        <Route path="/signup" element={<SignupPage />}> </Route>
       </Routes>
     </BrowserRouter>
     
